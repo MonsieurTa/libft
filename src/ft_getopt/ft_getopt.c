0fcccc5537cc1e90eaa8bc15e17c934cd35ce18b
William Ta <wta@student.42.fr>
2020-02-28 10:58:32 +0100

    fix: make ft_getopt usable

--- src/ft_getopt/ft_getopt.c
+++ src/ft_getopt/ft_getopt.c
@@ -6,7 +6,7 @@
 /*   By: wta <wta@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 14:31:42 by wta               #+#    #+#             */
-/*   Updated: 2020/01/17 15:29:25 by wta              ###   ########.fr       */
+/*   Updated: 2020/02/28 10:57:22 by wta              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,23 @@ int					g_optopt = 0;
 int					g_opterr = 1;
 char				*g_optarg = 0;
 
-static void	assign_optarg(int argc, char *const argv[], char *curr_opt)
+static void	assign_optarg(int argc, char *const argv[],
+	char *curr_c, int *nextchar)
 {
-	if (*(curr_opt + 2))
-		g_optarg = curr_opt + 2;
+	if (*(curr_c + 1))
+	{
+		g_optarg = curr_c + 1;
+		g_optind++;
+	}
 	else if (g_optind + 1 < argc)
-		g_optarg = (char*)&argv[g_optind + 1];
+	{
+		g_optind++;
+		g_optarg = argv[g_optind];
+	}
 	else
 		g_optarg = 0;
+	g_optind++;
+	*nextchar = 1;
 }
 
 static int	scan_opt(int argc, char *const argv[], const char *optstring,
@@ -34,24 +43,22 @@ static int	scan_opt(int argc, char *const argv[], const char *optstring,
 {
 	char	*optchar;
 	char	*curr_opt;
-	int		curr_c;
+	char	*curr_c;
 
 	optchar = argv[g_optind];
 	if (optchar[0] != '-')
 		return (0);
-	curr_c = (int)(optchar[*nextchar]);
-	if (!(curr_opt = ft_strchr(optstring, curr_c)))
+	curr_c = &optchar[*nextchar];
+	if (!(curr_opt = ft_strchr(optstring, *curr_c)))
 		return (0);
-	if (ft_strnequ(curr_opt + 1, "::", 2))
-		g_optarg = *(curr_opt + 3) ? curr_opt + 3 : 0;
-	else if (ft_strnequ(curr_opt + 1, ":", 1))
-		assign_optarg(argc, argv, curr_opt);
-	if ((size_t)++(*nextchar) >= ft_strlen(optchar))
+	if (ft_strnequ(curr_opt + 1, ":", 1))
+		assign_optarg(argc, argv, curr_c, nextchar);
+	else if ((size_t)++(*nextchar) >= ft_strlen(optchar))
 	{
 		g_optind++;
 		*nextchar = 1;
 	}
-	return (curr_c);
+	return (*curr_c);
 }
 
 static void	print_error(char *const argv[])
